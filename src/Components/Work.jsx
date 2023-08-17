ded4b4e660f871d03d70bcb911898e9bbbd8c668
You Know <111015172+OptimisticImpulse@users.noreply.github.com>
2023-08-17 13:54:07 -0400

    Add files via upload

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -12,7 +12,7 @@ const Work = () => {
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-white">
             Work I did
           </p>
-          <p className="py-6">Check out some of my 2023 ABC recent work</p>
+          <p className="py-6">Check out some of my recent work</p>
         </div>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* Grid Item */}
@@ -32,6 +32,29 @@ const Work = () => {
                   </button>
                 </a>
                 <a href="https://github.com/Jburdt/Final-project">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${golfapp})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effects */}
+            <div className="rounded-lg opacity-0 bg-indigo-500 group-hover:opacity-100 px-2">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React Js & Ruby on rails
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://youtu.be/n30qPNZy1qc">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/Jburdt/Golf-app-phase-4-project">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
