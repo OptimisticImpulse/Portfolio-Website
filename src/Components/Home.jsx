0451f88c7cb5f043ffc9d455cab009f4bf668ef4
You Know <111015172+OptimisticImpulse@users.noreply.github.com>
2023-08-17 14:29:21 -0400

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,13 +5,13 @@ const Home = () => {
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-white">Hi, my name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
-          Josh Burd
+          John Does
         </h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
-          I'm a Full Stack Developer.
+          I'm a Full Stack Developer II.
         </h2>
         <p className="text-[#8893b0] py-4 max-w-[700px]">
-          I'm a full-stack developer specializing in building (and occasionally
+          I'm 5165161813151665161 a full-stack developer specializing in building (and occasionally
           designing) exceptional digital experiences. Currently, I'm focused on
           building responsive full-stack web applications.
         </p>
