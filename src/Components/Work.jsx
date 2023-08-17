65afb7366ff3436316d1a5f92b15b1503d99f443
You Know <111015172+OptimisticImpulse@users.noreply.github.com>
2023-08-17 12:21:09 -0400

    Update Work.jsx

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -17,6 +17,29 @@ const Work = () => {
         <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-4">
           {/* Grid Item */}
           <div
+            style={{ backgroundImage: `url(${calc})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effects */}
+            <div className="rounded-lg opacity-0 bg-indigo-500 group-hover:opacity-100 px-2">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Javascript Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://medium.com/@jburdt/basic-calculator-app-fbc966ce6c2a">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Blog
+                  </button>
+                </a>
+                <a href="https://github.com/Jburdt/calculator-app.git">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
             style={{ backgroundImage: `url(${MovieReview})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
