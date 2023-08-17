1a1085b04ccb4d3c13054e536cde2860a5fd1aac
You Know <111015172+OptimisticImpulse@users.noreply.github.com>
2023-08-17 14:01:15 -0400

    Update Work.jsx

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -12,7 +12,7 @@ const Work = () => {
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-white">
             Work I did
           </p>
-          <p className="py-6">Check out some of my recent work</p>
+          <p className="py-6">Check out some of my 2023 recent work</p>
         </div>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* Grid Item */}
